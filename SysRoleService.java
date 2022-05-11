98edf21e43953fb4331f9c237e45618bd39b6dd2
Lverning <74942379+lverning@users.noreply.github.com>
2022-05-11 23:28:15 +0800

    Add files via upload

---
+++ SysRoleService.java
@@ -0,0 +1,16 @@
+package com.service;
+
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.springclout.entities.SysRole;
+import com.springclout.entities.SysUser;
+
+/**
+ * @author 吕二宁
+ * @version 1.0
+ * @description: TODO
+ * @date 2022/5/6 11:38
+ */
+public interface SysRoleService extends IService<SysRole> {
+
+}
